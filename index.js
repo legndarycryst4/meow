54c3fb0703858fe92f747c37a1e306cd0e4ef713
Elizabeth <107285224+legndarycryst4@users.noreply.github.com>
2025-12-11 00:37:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express  = require('express');
+const { resolve } = require('path');
+const app = express();
+const port = 3000;
+//const path = resolve(__dirname, 'public');
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'index.html'));
+});
+app.get('/MineTop', (req, res) => {
+  res.sendFile(resolve(__dirname, 'server/MineTop.html'));
+});
+app.get('/AllayRival', (req, res) => {
+  res.sendFile(resolve(__dirname, 'server/AllayRival.html'));
+});
+app.get('/minerel', (req, res) => {
+  res.sendFile(resolve(__dirname, 'server/minerel.html')); });
+app.get('/AcentraMC', (req, res) => {
+  res.sendFile(resolve(__dirname, 'server/AcentraMC.html'));
+});
+app.get('/ArcadeHUB', (req, res) => {
+  res.sendFile(resolve(__dirname, 'server/ArcadeHUB.html'));
+});
+app.get('/badge.css', (req, res) => {
+  res.sendFile(resolve(__dirname, 'static/badge.css'));
+});
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
